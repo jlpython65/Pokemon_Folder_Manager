998b09b1d5c67bd66676e33c09208961943fabee
jlpython65 <105390022+jlpython65@users.noreply.github.com>
2022-06-09 13:08:39 -0400

    Update #(revised)Pokemon_File_Manager.py

--- #(revised)Pokemon_File_Manager.py
+++ #(revised)Pokemon_File_Manager.py
@@ -10,27 +10,6 @@ from tokenize import Number
 
 src_path = r"C:\Users\JASON LEE\Documents\1Python\Pokemon_Game\gen8 - Copy"
 
-def delete_back_shinies_gigashiny():
-    regex = r'.+?sb.png|.+?b.png|.+?s.png' #.+? is satisfied after the sprite number so regex moves onto matching the sb part. Actually greedy or not greedy, wouldn't make a difference. Greedy, the dot would bakc trakc
-
-    glob.glob(src_path, recursive=False) #recursive false becasue I want to delete sprites in THIS folder
-
-
-    for sprite in glob.iglob(src_path+r"/*.png"): #iglob is an iterator version of glob
-        if re.search(regex, sprite):
-            print(sprite)
-            os.remove(sprite)
-
-def rename_new_forms():
-    regex = r'.+?\_\d\d+?' #.+? is satisfied after the sprite number so regex moves onto matching the sb part. Actually greedy or not greedy, wouldn't make a difference. Greedy, the dot would bakc trakc
-
-    glob.glob(src_path, recursive=False) #recursive false becasue I want to delete sprites in THIS folder
-
-
-    for sprite in glob.iglob(src_path+r"/*.png"): #iglob is an iterator version of glob
-        if re.search(regex, sprite):    
-            print(sprite)
-            os.remove(sprite)
 
 def rename_pokemon():
     list_of_pokemon = input("please insert list of pokemon here").split()
